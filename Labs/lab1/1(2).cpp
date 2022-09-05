6f4a920361e1de68934a0604620c711324a631c4
VaraiONI <varaioni@tutanota.com>
2022-09-05 17:29:19 +0500

    labs

--- Labs/lab1/1(2).cpp
+++ Labs/lab1/1(2).cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
     double n;
-    double m = 5;
+    double m;
     cout << "\n Введите n ";
     cin >> n;
+    cout << "\n Введите m ";
+    cin >> m;
     cout << "\nЗначение выражения --m-++n= " << (--m - ++n);
     cout << "\nЗначение выражения m*n<n++ = " << (m * n < n++);
     cout << "\nЗначение выражения n-->m++ = " << (n-- > m++);
     cout << "\n";
 
-        system("pause");
+    system("pause");
     return 0;
 }
