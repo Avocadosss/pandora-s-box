32ed6b585d76b6e3829d5c575a3e040eac6d1f57
VaraiONI <67595162+Avocadosss@users.noreply.github.com>
2022-10-21 16:29:20 +0500

    Add files via upload

---
+++ Labs/Квадрат/1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int side;
+ 
+    cout << "vvedite razmer storon kvadrata: ";
+    cin >> side;
+    cout << endl;
+ 
+    for(int i = 0; i < side; i++)
+    {
+        for(int j = 0; j < side; j++)
+        {
+            if(i==0 || j==0 || i==side-1 || j==side-1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    system("pause");
+    return 0;
+}
